ff660c9ab67f1c26442bc99b04d4062c7c544369
637sahil <Sahil.20B1531007@abes.ac.in>
2023-07-21 17:50:20 +0530

    second commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,12 +14,12 @@ function Card({id, image,info,price,name, removeTour})  {
         
         <div className="card">
 
-            <img src={image} className="image"></img>
+            <img src={image} className="image" alt={name}></img>
 
             <div className="tour-info">
                 <div className="tour-details">
                     <h4 className="tour-price">₹ {price}</h4>
-                    <h4 className="tour-name">{name}< /h4>
+                    <h4 className="tour-name">{name}</h4>
                 </div>
 
                 <div className="description">
